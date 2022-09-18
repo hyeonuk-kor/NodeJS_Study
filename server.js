1fd76c998e29219383e2cedf39f943ba2dadde83
hyeonuk-kor <mrseos@naver.com>
2022-09-19 00:24:01 +0900

    Update server.js

--- server.js
+++ server.js
@@ -15,3 +15,7 @@ app.get("/pet", function (요청, 응답) {
 app.get("/beauty", function (요청, 응답) {
 	응답.send("뷰티용품사세요");
 });
+
+app.get("/", function (요청, 응답) {
+	응답.sendFile(__dirname + "/index.html");
+});
