82192e614fa2d881b6fd8468502220afd78f9590
hyeonuk-kor <mrseos@naver.com>
2022-09-19 23:35:07 +0900

    Update server.js

--- server.js
+++ server.js
@@ -1,19 +1,32 @@
-const fs = require('fs');
+const fs = require("fs");
 // 서버를 띄우기 위한 기본 셋팅
 const express = require("express"); //express 라이브러리 셋팅
 const app = express(); //객체 만들기
-const bodyParser = require('body-parser')
-app.use(bodyParser.urlencoded({ extended: true }))
-const MongoClient = require('mongodb').MongoClient
+const bodyParser = require("body-parser");
+app.use(bodyParser.urlencoded({ extended: true }));
+const MongoClient = require("mongodb").MongoClient;
 
-const jsonfile = fs.readFileSync('./password.json');
+const jsonfile = fs.readFileSync("./password.json");
 const password = JSON.parse(jsonfile).value;
-MongoClient.connect('mongodb+srv://hyeonuk:' + password + '@cluster0.2onwa9n.mongodb.net/?retryWrites=true&w=majority', function (에러, client) {
-	if (에러) return console.log(에러)
-	app.listen(8080, function () {
-		console.log('listening on 8080')
-	})
-})
+let db;
+MongoClient.connect(
+	"mongodb+srv://hyeonuk:" +
+		password +
+		"@cluster0.2onwa9n.mongodb.net/?retryWrites=true&w=majority",
+	function (에러, client) {
+		if (에러) return console.log(에러);
+		db = client.db("petpy_db");
+		db.collection("post").insertOne(
+			{ 이름: "John", 나이: 20 },
+			function (에러, 결과) {
+				console.log("저장완료");
+			}
+		);
+		app.listen(8080, function () {
+			console.log("listening on 8080");
+		});
+	}
+);
 
 // 누군가 /pet으로 방문하면 pet관련된 안내문을 띄워주자.
 app.get("/pet", function (요청, 응답) {
@@ -31,9 +44,12 @@ app.get("/write", function (요청, 응답) {
 	응답.sendFile(__dirname + "/write.html");
 });
 
-app.post('/add', function (요청, 응답) {
-	console.log(요청.body.title);
-	console.log(요청.body.date);
-	응답.send('전송완료')
+app.post("/add", function (요청, 응답) {
+	응답.send("전송완료");
+	db.collection("post").insertOne(
+		{ 제목: 요청.body.title, 날짜: 요청.body.date },
+		function () {
+			console.log("저장완료");
+		}
+	);
 });
-
