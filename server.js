fc183d060c5d76e508e932b62213925fb6d20d44
hyeonuk-kor <mrseos@naver.com>
2022-09-21 00:56:51 +0900

    Update server.js
    Update list.ejs
    추가 삭제 수정 구현

--- server.js
+++ server.js
@@ -5,15 +5,15 @@ const app = express(); //객체 만들기
 const bodyParser = require("body-parser");
 app.use(bodyParser.urlencoded({ extended: true }));
 const MongoClient = require("mongodb").MongoClient;
-app.set('view engine', 'ejs');
+app.set("view engine", "ejs");
 
 const jsonfile = fs.readFileSync("./password.json");
 const password = JSON.parse(jsonfile).value;
 let db;
 MongoClient.connect(
 	"mongodb+srv://hyeonuk:" +
-	password +
-	"@cluster0.2onwa9n.mongodb.net/?retryWrites=true&w=majority",
+		password +
+		"@cluster0.2onwa9n.mongodb.net/?retryWrites=true&w=majority",
 	function (에러, client) {
 		if (에러) return console.log(에러);
 		db = client.db("petpy_db");
@@ -41,17 +41,49 @@ app.get("/write", function (요청, 응답) {
 
 app.post("/add", function (요청, 응답) {
 	응답.send("전송완료");
-	db.collection("post").insertOne(
-		{ 제목: 요청.body.title, 날짜: 요청.body.date },
-		function () {
-			console.log("저장완료");
+	db.collection("counter").findOne(
+		{ name: "게시물갯수" },
+		function (에러, 결과) {
+			console.log(결과.totalPost);
+			var 총게시물갯수 = 결과.totalPost;
+			db.collection("post").insertOne(
+				{
+					_id: 총게시물갯수 + 1,
+					제목: 요청.body.title,
+					날짜: 요청.body.date,
+				},
+				function () {
+					console.log("저장완료");
+					db.collection("counter").updateOne(
+						{ name: "게시물갯수" },
+						{ $inc: { totalPost: 1 } }, // operator 써야함
+						function (에러, 결과) {
+							if (에러) {
+								return console.log(에러);
+							}
+						}
+					);
+				}
+			);
 		}
 	);
 });
 
-app.get('/list', function (요청, 응답) {
-	db.collection('post').find().toArray(function (에러, 결과) {
-		console.log(결과);
-		응답.render('list.ejs', { posts: 결과 });
+app.get("/list", function (요청, 응답) {
+	db.collection("post")
+		.find()
+		.toArray(function (에러, 결과) {
+			console.log("++\n");
+			console.log(결과);
+			응답.render("list.ejs", { posts: 결과 });
+		});
+});
+
+app.delete("/delete", function (요청, 응답) {
+	console.log(요청.body);
+	요청.body._id = parseInt(요청.body._id);
+	db.collection("post").deleteOne(요청.body, function (에러, 결과) {
+		console.log("삭제완료");
+		응답.status(200).send({ message: "성공했습니다." });
 	});
 });
