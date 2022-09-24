114f9c3d699e7cc9087288ef05ce0ee5dbb98ade
hyeonuk-kor <mrseos@naver.com>
2022-09-24 21:28:41 +0900

    라우트 실습
    라우트 실습

--- routes/shop.js
+++ routes/shop.js
@@ -1,8 +1,8 @@
 var router = require("express").Router();
-router.get("/shop/shirts", function (요청, 응답) {
+router.get("/shirts", function (요청, 응답) {
 	응답.send("셔츠 파는 페이지입니다.");
 });
-router.get("/shop/pants", function (요청, 응답) {
+router.get("/pants", function (요청, 응답) {
 	응답.send("바지 파는 페이지입니다.");
 });
-module.exports = router;
+module.exports = router; // 다른 곳에서 shop.js를 쓰고싶을 때 내보내고 싶은 변수이름
--- server.js
+++ server.js
@@ -221,3 +221,7 @@ app.delete("/delete", function (요청, 응답) {
 		응답.status(200).send({ message: "성공했습니다." });
 	});
 });
+
+app.use("/shop", require("./routes/shop.js")); // 미들웨어를 쓰고 싶을 때 use 사용, / 경로로 요청했을 때 라우터 적용~
+
+app.use("/board/sub", require("./routes/board.js"));
