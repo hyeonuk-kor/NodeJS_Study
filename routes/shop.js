3a2eb881e589c726ae597f00244aa6cc6ee46200
hyeonuk-kor <mrseos@naver.com>
2022-09-25 01:37:03 +0900

    Update shop.js

--- routes/shop.js
+++ routes/shop.js
@@ -13,4 +13,4 @@ router.get("/shirts", function (요청, 응답) {
 router.get("/pants", function (요청, 응답) {
 	응답.send("바지 파는 페이지입니다.");
 });
-module.exports = router; // 다른 곳에서 shop.js를 쓰고싶을 때 내보내고 싶은 변수이름
+module.exports = router;
