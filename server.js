f6ebe438d9992bf49907ed0dbe931ce8082334a8
hyeonuk-kor <mrseos@naver.com>
2022-09-25 15:27:24 +0900

    소켓 통신 추가

--- server.js
+++ server.js
@@ -6,6 +6,11 @@ const bodyParser = require("body-parser");
 app.use(bodyParser.urlencoded({ extended: true }));
 const MongoClient = require("mongodb").MongoClient;
 const methodOverride = require("method-override");
+
+const http = require("http").createServer(app);
+const { Server } = require("socket.io");
+const io = new Server(http);
+
 app.use(methodOverride("_method"));
 app.set("view engine", "ejs");
 app.use("/public", express.static("public"));
@@ -14,7 +19,7 @@ let db;
 MongoClient.connect(process.env.DB_URL, function (에러, client) {
 	if (에러) return console.log(에러);
 	db = client.db("petpy_db");
-	app.listen(8080, function () {
+	http.listen(8080, function () {
 		console.log("listening on " + 8080);
 	});
 });
@@ -259,4 +264,95 @@ app.post("/upload", upload.single("profile"), (요청, 응답) => {
 
 app.get("/image/:imageName", (요청, 응답) => {
 	응답.sendFile(__dirname + "/public/image/" + 요청.params.imageName);
+});
+const { ObjectId } = require("mongodb");
+app.post("/chatroom", 로그인했니, function (요청, 응답) {
+	console.log("요청들어옴");
+	var 저장할거 = {
+		title: "무슨무슨채팅방",
+		member: [ObjectId(요청.body.당한사람id), 요청.user._id],
+		date: new Date(),
+	};
+	console.log(저장할거);
+	db.collection("chatroom")
+		.insertOne(저장할거)
+		.then(function (결과) {
+			응답.send("저장완료");
+		});
+});
+
+app.get("/chat", 로그인했니, function (요청, 응답) {
+	db.collection("chatroom")
+		.find({ member: 요청.user._id })
+		.toArray()
+		.then((결과) => {
+			console.log(결과);
+			응답.render("chat.ejs", { data: 결과 });
+		});
+});
+
+app.post("/message", 로그인했니, function (요청, 응답) {
+	var 저장할거 = {
+		parent: 요청.body.parent,
+		content: 요청.body.content,
+		userid: 요청.user._id,
+		date: new Date(),
+	};
+	console.log(저장할거);
+	db.collection("message")
+		.insertOne(저장할거)
+		.then(() => {
+			console.log("db저장성공");
+		});
+});
+
+app.get("/message/:parentid", 로그인했니, function (요청, 응답) {
+	응답.writeHead(200, {
+		Connection: "keep-alive",
+		"Content-Type": "text/event-stream;charset=utf-8",
+		"Cache-Control": "no-cache",
+	});
+
+	db.collection("message")
+		.find({ parent: 요청.params.parentid })
+		.toArray()
+		.then((결과) => {
+			console.log(결과);
+			응답.write("event: test\n");
+			응답.write(`data: ${JSON.stringify(결과)}\n\n`);
+		});
+
+	const 찾을문서 = [
+		{ $match: { "fullDocument.parent": 요청.params.parentid } },
+	];
+
+	const changeStream = db.collection("message").watch(찾을문서);
+	changeStream.on("change", (result) => {
+		console.log("hnkjhkhkjhkj-----");
+		console.log(result.fullDocument);
+		var 추가된문서 = [result.fullDocument];
+		응답.write("event: test\n");
+		응답.write(`data: ${JSON.stringify(추가된문서)}\n\n`);
+	});
+});
+
+app.get("/socket", function (요청, 응답) {
+	응답.render("socket.ejs");
+});
+
+io.on("connection", function (socket) {
+	console.log("유저접속됨");
+	console.log(socket.id); // 유저 구분!
+
+	socket.on("joinroom", function (data) {
+		socket.join("room1");
+	});
+
+	socket.on("room1-send", function (data) {
+		io.to("room1").emit("broadcast", data);
+	});
+	socket.on("user-send", function (data) {
+		io.emit("broadcast", data);
+		console.log(data);
+	});
 });
